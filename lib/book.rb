f8513ae64f97a9e1d5437f3ce10cbdafe7b1e22a
Sherry <sherryptk@gmail.com>
2017-11-13 21:04:19 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,5 +1,5 @@
 class Book
-  attr_accessor :author, :page_count 
+  attr_accessor :author, :page_count
   attr_reader :title, :genre
 
   GENRES = []
@@ -14,6 +14,6 @@ class Book
 
   def genre=(genre)
       @genre = genre
-      GENRES << genre 
-    end
+      GENRES << genre
+  end
 end
